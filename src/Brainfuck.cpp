f2a957ad00d8fe0b9a89bfe9b40ab7b2602b338a
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-04-20 21:59:17 +0200

    🎈 Tests

--- src/Brainfuck.cpp
+++ src/Brainfuck.cpp
@@ -1,15 +1,40 @@
-﻿// Brainfuck.cpp : définit le point d'entrée de l'application.
-//
+﻿/*
+Brainfuck.cpp :
+ 
+Unit test
+*/
+
+#include <thread>
 
 #include "Brainfuck.h"
+
+
+
+#include <filesystem>
+namespace fs = std::filesystem;
 
 using namespace std;
 
 int main()
 {
-	Interpreter i = Interpreter();
+	Interpreter i1 = Interpreter();
 
-	//i.fromString(">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++ .------------. > ++++++[<++++++++ + >-]<+.<.++ + .------.--------.>> > ++++[<++++++++>-] < +.");
-	i.fromString(">++++++++++>+>+[[+++++[>++++++++<-]>.<++++++[>--------<-]+<<<]>.>>[[-]<[>+<-]>>[<<+>+>-]<[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>[-]>+>+<<<-[>+<-]]]]]]]]]]]+>>>]<<<]");
+	std::ifstream e1("../../../../Brainfuck/HelloWorld.bf");
+	i1.run(e1);
+
+	std::cout << "\n";
+
+	Interpreter i2 = Interpreter();
+
+	std::ifstream e2("../../../../Brainfuck/Input.bf");
+	i2.run(e2);
+
+	std::cout << "\n";
+
+	Interpreter i3 = Interpreter();
+
+	std::ifstream e3("../../../../Brainfuck/Fib.bf");
+	i3.run(e3);
+
 	return 0;
 }
